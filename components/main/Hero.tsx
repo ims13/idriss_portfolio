569f59d754923a9617f80741243715b50c97dd4f
ims13 <87872665+ims13@users.noreply.github.com>
2024-05-15 13:48:46 -0400

    navbar updated

--- components/main/Hero.tsx
+++ components/main/Hero.tsx
@@ -14,7 +14,7 @@ const Hero = () => {
         muted
         loop
         playsInline
-        className='rotate-180 absolute top-[-340px] left-0 z-[1] w-full h-full object-cover'
+        className='rotate-180 absolute top-[-380px] left-0 z-[1] w-full h-full object-cover'
         >
             <source src='/blackhole.webm' type='video/webm'/>
 
