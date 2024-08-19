91ca9ab626622b339718d215a7d896b3c1138df6
ims13 <87872665+ims13@users.noreply.github.com>
2024-08-18 20:23:17 -0400

    test3 using .mp4 formats

--- utils/detectMobile.ts
+++ utils/detectMobile.ts
@@ -1,8 +1,10 @@
-// utils/detectMobile.ts
+// utils/detectMobile.ts should detect a mobile
 export const isMobileDevice = () => {
 
     return (
       typeof window !== "undefined" &&
       /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)
     );
+    
+    
   };
